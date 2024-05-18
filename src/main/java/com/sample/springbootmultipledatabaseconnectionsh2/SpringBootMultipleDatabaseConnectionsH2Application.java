b082610cdca72c809ed3db55a3a31a163af489ff
saisampaths60 <saisampathkatru@gmail.com>
2024-05-18 16:45:28 +0530

    initial commit

---
+++ src/main/java/com/sample/springbootmultipledatabaseconnectionsh2/SpringBootMultipleDatabaseConnectionsH2Application.java
@@ -0,0 +1,13 @@
+package com.sample.springbootmultipledatabaseconnectionsh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMultipleDatabaseConnectionsH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMultipleDatabaseConnectionsH2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sample/springbootmultipledatabaseconnectionsh2/SpringBootMultipleDatabaseConnectionsH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.springbootmultipledatabaseconnectionsh2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMultipleDatabaseConnectionsH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
